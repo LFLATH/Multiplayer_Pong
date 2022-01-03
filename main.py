a115cae62d88815845ad4eeb824ffbbcbf56a0fe
Ludwig Lechtreck <ludwiglechtreck@gmail.com>
2022-01-03 17:21:36 -0500

    Added the ball, the second player, and collision detection to the project

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 from pygame.locals import *
 import sys
+from random import randint
 #Starts pygame
 pygame.init()
-#Creates the display with a width of 300 by 300 pixles
+#Creates the display with a width of 640 by 480 pixles
 screen = pygame.display.set_mode((640,480))
 #Defines FPS as our game clock
 FramesPerSecond = 60
@@ -15,9 +16,12 @@ WHITE = (255, 255, 255)
 
 #Changes the name our window to Pong
 pygame.display.set_caption("Pong")
+pointwinner = 'r'
 
-#Player Class
-class Player(pygame.sprite.Sprite):
+
+
+#Player1 Class
+class Player1(pygame.sprite.Sprite):
     #Init
     def __init__(self):
         super().__init__()
@@ -26,8 +30,6 @@ class Player(pygame.sprite.Sprite):
         self.image.fill(WHITE)
         pygame.draw.rect(self.image,WHITE,pygame.Rect(5, 5, 4, 28))
         self.rect = self.image.get_rect()
-        self.x = 20
-        self.y = 20
     #Movement of the player
     def update(self):
         #Gets the key stroke
@@ -38,13 +40,89 @@ class Player(pygame.sprite.Sprite):
         elif key[pygame.K_UP] and self.rect.y > 0: # up key
             self.rect.y -= dist # move up
 
+class Player2(pygame.sprite.Sprite):
+    #Init
+    def __init__(self):
+        super().__init__()
+        #Creates the player and sets its starting point
+        self.image = pygame.Surface([4, 28])
+        self.image.fill(WHITE)
+        pygame.draw.rect(self.image,WHITE,pygame.Rect(5, 5, 4, 28))
+        self.rect = self.image.get_rect()
+    #Movement of the player
+    def update(self):
+        #Gets the key stroke
+        key = pygame.key.get_pressed()
+        dist = 2 # distance moved in 1 frame,
+        if key[pygame.K_s] and self.rect.y  < 480 - 28 : # down key
+            self.rect.y += dist # move down
+        elif key[pygame.K_w] and self.rect.y > 0: # up key
+            self.rect.y -= dist # move up
+
+
+
+
+class Ball(pygame.sprite.Sprite):
+    #init
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface([6, 6])
+        self.image.fill(WHITE)
+        pygame.draw.circle(self.image,WHITE, (6 // 2, 6 //2), 6)
+        self.rect = self.image.get_rect()
+        generator = randint(0,1)
+        pointwinner = 'r'
+        if pointwinner == 'r':
+            if generator == 1:
+                self.rect.x = 225
+                self.rect.y = 20
+            else:
+                self.rect.x = 225
+                self.rect.y = 460
+        if pointwinner == 'l':
+            if generator == 1:
+                self.rect.x = 215
+                self.rect.y = 20
+            else:
+                self.rect.x = 215
+                self.rect.y = 460
+#Function to calculate the movement of the ball
+    def update(self, collisionwithbar, changeinx, changeiny):
+        if self.rect.x == 640:
+            self.rect.kill()
+            pointwinner = 'r'
+        if self.rect.x == 0:
+            self.rect.kill()
+            pointwinner = 'l'
+
+
+
+
+
+
+
+
+
+
+
+
 #Init the sprite
-P1 = Player()
+P1 = Player1()
 P1.rect.x = 20
 P1.rect.y = 20
+P2 = Player2()
+P2.rect.x = 620
+P2.rect.y = 20
+ball = Ball()
+ball.rect.x = 225
+ball.rect.y = 20
+ball_path = [2,2]
 #Create Sprite List
 player_sprites_list = pygame.sprite.Group()
 player_sprites_list.add(P1)
+player_sprites_list.add(P2)
+ball_sprite_list = pygame.sprite.Group()
+ball_sprite_list.add(ball)
 #Program loop
 while True:
     for event in pygame.event.get():
@@ -53,13 +131,22 @@ while True:
         sys.exit()
     #Updates the position of the player
     player_sprites_list.update()
-    
+    #Collision Detection
+    ball.rect.move_ip(ball_path)
+    if ball.rect.x > 640 or ball.rect.x < 0:
+        ball.kill()
+    if ball.rect.y < 0 or ball.rect.y > 480:
+        ball_path[1] = -ball_path[1]
+    if P1.rect.colliderect(ball.rect)== 1 or P2.rect.colliderect(ball.rect) == 1:
+        ball_path[0] = -ball_path[0]
+        
+
 
     #Makes the backround black
     screen.fill(BLACK) 
     #Draws our player
     player_sprites_list.draw(screen) 
-   
+    ball_sprite_list.draw(screen)
           
     
     
